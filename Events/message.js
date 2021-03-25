31c3384b40e51123f4463ff60bcad1feedf9be4e
Dinav <45940386+Dinav69@users.noreply.github.com>
2021-03-25 09:10:28 +0400

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -1,47 +1,20 @@
 const Discord = require('discord.js');
 const emoji = require('../Utils/emoji.json');
 const config = require('../config.json');
-const escapeRegex = (str) => str.replace(/[.*+?^${}()|[\]\\]/g, `\\$&`);
 
 module.exports = async (client, message) => {
-    if (message.author.bot || !message.guild || message.webhookID) return;
-
-    if (message.content === `<@${client.user.id}>`){
-
-    const embed = new Discord.MessageEmbed()
-    .setTitle('Did you ping me ??')
-    .setThumbnail(client.user.displayAvatarURL({dynamic: true, size: 1024}))
-    .setDescription(`Aight I'm ${client.user.username} I'm a multi purpose bot my prefix is \`${config.Prefix}\` try \`${config.Prefix}help\` to get to know me more\n\n[Add Me](${config.Invite}) | [Join Server](${config.Server})`)
-    .setTimestamp()
-
-     message.channel.send(embed)
-    }
-    if (message.content === `<@!${client.user.id}>`){
-
-    const embed = new Discord.MessageEmbed()
-    .setTitle('Did you ping me ??')
-    .setThumbnail(client.user.displayAvatarURL({dynamic: true, size: 1024}))
-    .setDescription(`Aight I'm ${client.user.username} I'm a multi purpose bot my prefix is \`${config.Prefix}\` try \`${config.Prefix}help\` to get to know me more\n\n[Add Me](${config.Invite}) | [Join Server](${config.Server})`)
-    .setTimestamp()
-
-    message.channel.send(embed)
-    }
+    if (message.author.bot || !message.guild || message.webhookID || message.channel.type === 'dm') return;
 
     const prefix = config.Prefix;
+    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${prefix.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')})\\s*`);
 
-    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
-    if (!prefixRegex.test(message.content)) return;
-    const [, matchedPrefix] = message.content.match(prefixRegex);
-    const args = message.content.slice(matchedPrefix.length).trim().split(/ +/);
+    if (prefixRegex.test(message.content)) {
   
-    if (!message.content.startsWith(prefix)) return;
+    const [, match] = message.content.match(prefixRegex);
 
-    if (!message.member) message.member = await message.guild.fetchMember(message);
+    const args = message.content.slice(match.length).trim().split(/ +/g);
 
-    
     const cmd = args.shift().toLowerCase();
-    
-    if (cmd.length === 0) return;
     
     let command = client.commands.get(cmd);
    
@@ -50,4 +23,17 @@ module.exports = async (client, message) => {
     if (command) 
     
     command.run(client, message, args);
+
+    if (message.content === `<@${client.user.id}>` || message.content === `<@!${client.user.id}>`) {
+
+    const embed = new Discord.MessageEmbed()
+    .setTitle('Did you ping me ??')
+    .setThumbnail(client.user.displayAvatarURL({dynamic: true, size: 1024}))
+    .setDescription(`Aight I'm ${client.user.username} I'm a multi purpose bot my prefix is \`${config.Prefix}\` try \`${config.Prefix}help\` to get to know me more\n\n[Add Me](${config.Invite}) | [Join Server](${config.Server}) | [Source Code](${config.Github})`)
+    .setTimestamp()
+
+    message.channel.send(embed)
+
+    }
+    }
 }
