52ef6f138e85770067ecdb365cd106050a82e82c
Dinav <45940386+Dinav69@users.noreply.github.com>
2021-03-25 09:14:49 +0400

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -1,9 +1,7 @@
-const config = require('../config.json');
-
 module.exports = async (client) => {
         
     process.on('unhandledRejection', err => console.log(err))
     console.log(`${client.user.tag} is now conneted to Discord !!`)
-    client.user.setActivity(`${config.Prefix}help | ${client.guilds.cache.size} Servers`, {type: "WATCHING"})
+    client.user.setActivity(`@${client.user.username}`, {type: "LISTENING"})
     
 }
