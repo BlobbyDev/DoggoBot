63f36d1607da77e194b257ae2db10bed9fc43296
Dinav <45940386+Dinav69@users.noreply.github.com>
2021-04-01 12:25:20 +0400

    Update message.js

--- Events/message.js
+++ Events/message.js
@@ -1,7 +1,5 @@
 const Discord = require('discord.js');
 const config = require('../config.json');
-const ms = require('ms');
-const emoji = require('../Utils/emoji.json');
 
 module.exports = async (client, message) => {
     
@@ -20,34 +18,22 @@ module.exports = async (client, message) => {
     }
 
     const prefix = config.Prefix;
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    const Timeout = new Set()
   
     if (!message.content.toLowerCase().startsWith(prefix)) return;
 
     if (!message.member) message.member = await message.guild.fetchMember(message);
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
     
     if (cmd.length === 0) return;
     
     let command = client.commands.get(cmd);
-
-    if (command) {
-
-        if(command.cooldown) {
-            if (Timeout.has(`${message.author.id}${command.name}`)) {
-                return message.reply(`${emoji.Error} You can only use this command every \`${ms(command.cooldown)}\` !!`)
-            } else {
-                Timeout.add(`${message.author.id}${command.name}`)
-                setTimeout(() => {
-                    Timeout.delete(`${message.author.id}${command.name}`)
-                }, command.cooldown);
-            }
-        }
-
-        command.run(client, message, args);
-    }
    
     if (!command) command = client.commands.get(client.aliases.get(cmd));
    
+    if (command) 
+    
+    command.run(client, message, args);
+
 }
