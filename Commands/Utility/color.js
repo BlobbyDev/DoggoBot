b659d0bcdbc4ee01320b9582b0fc01c63734fba3
Blobby <45940386+BlobbyDev@users.noreply.github.com>
2021-06-08 08:47:39 +0400

    Add files via upload

---
+++ Commands/Utility/color.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const emoji = require('../../Utils/emoji.json');
+const config = require('../../config.json');
+const DIG = require("discord-image-generation");
+
+module.exports = {
+    name: "color",
+    aliases: ['colour'],
+    category: "Image",
+    description: "Gives a colour that you have provided",
+    example: `${config.Prefix}colour #FF0000`,
+
+    run: async (client, message, args) => {
+
+        if(!args[0])
+        return message.reply(`${emoji.Error} Provide a hex`);
+
+        try {
+
+        let img = await new DIG.Color().getImage(args[0]);
+
+        let attach = new Discord.MessageAttachment(img, `${args[0]}.png`);
+
+        message.channel.send(attach)
+        } catch (err) {
+            message.reply(`${emoji.Error} Cannot give colour ${args[0]}`);
+        }
+    }
+}
