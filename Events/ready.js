2f001c317650dd25a7d2d41c92f419c3221ae0e0
Blobby <45940386+BlobbyDev@users.noreply.github.com>
2021-10-19 11:32:49 -0700

    Update ready.js

--- Events/ready.js
+++ Events/ready.js
@@ -7,10 +7,8 @@ module.exports = async (client) => {
     usersCount += (await guild[1].members.fetch()).size
     }
         
-    const channel = client.channels.cache.get('841516179876478997')
     process.on('unhandledRejection', err => console.log(err))
     client.user.setActivity(`${config.Prefix}help | ${client.guilds.cache.size} Servers`, {type: "LISTENING"}, {status: "dnd"})
     await console.log(`${client.user.tag} is now conneted to Discord !! Cached ${usersCount} Users`)
-    await channel.send(`<a:online:814401275587592222> | **${client.user.username}** is ready`)
     
 }
